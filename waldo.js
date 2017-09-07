8a096d3ef522bb90a5b21d520a3db9aafb3c463a
BugleJones <spencerjhamilton@gmail.com>
2017-09-07 18:13:21 +0000

    Refeactored for second exercise, using forEach loop

--- waldo.js
+++ waldo.js
@@ -1,36 +1,37 @@
-// The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
-  }
-}
-
-function actionWhenFound(x) {
-  console.log("Found Waldo at index " + x + "!");
-}
-
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-
-
+//First assignment:
 // // The second argument/parameter is expected to be a function
 // function findWaldo(arr, found) {
-//   arr.forEach(function(names, cb) {
-//     if (arr.names === "Waldo") {
-//       found(names, arr,  );  // execute callback
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
 //     }
-//   })
+//   }
 // }
-//   // for (var i = 0; i < arr.length; i++) {
-//   //   if (arr[i] === "Waldo") {
-//   //     found();   // execute callback
-//   //   }
-//   // }
 
-// function actionWhenFound() {
-//   console.log("Found him!");
+// function actionWhenFound(x) {
+//   console.log("Found Waldo at index " + x + "!");
 // }
 
 // findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+//Second assignment:
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(function(names, index) {
+    if (names === "Waldo") {
+      found(index);  // execute callback
+    }
+  });
+}
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found();   // execute callback
+  //   }
+  // }
+
+function actionWhenFound(index) {
+  console.log("Found him! at index: " + index);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
